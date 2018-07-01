c9f43054108c8283a5a506eb6451edd91dd34dc7
visig <visig@protonmail.ch>
2018-07-01 18:10:33 +0800

    add: __version__ variable in top level

--- src/unoccupied/__init__.py
+++ src/unoccupied/__init__.py
@@ -4,6 +4,7 @@ from .main import unoccupied
 from .main import UnoccupiedNameNotFound
 from .namefinder import NumberNameFinder
 from .namefinder import FileNameFinder
+from .info import VERSION as __version__  # NOQA
 
 __all__ = ['unoccupied',
            'UnoccupiedNameNotFound',
